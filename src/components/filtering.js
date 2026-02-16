799eb52ff4cf7d2d515b7bbe6d2045b7072a3e29
Xorogaz <74246712+Xorogaz@users.noreply.github.com>
2026-02-16 19:29:59 +0700

    first commit

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,40 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
-
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes).forEach((elementName) => {
+        elements[elementName].append(
+            ...Object.values(indexes[elementName])
+                      .map(name => {
+                          const option = document.createElement('option');
+                          option.value = name;
+                          option.textContent = name;
+                          return option;
+                      })
+        )
+    });
+
+    const compare = createComparison(defaultRules);
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const field = action.dataset.field;
+            const input = action.parentElement.querySelector('input');
+            if (input) {
+                input.value = '';
+            }
+            if (field) {
+                state[field] = '';
+            }
+        }
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        const filterState = {
+            ...state,
+            total: [
+                state.totalFrom ? Number(state.totalFrom) : undefined,
+                state.totalTo ? Number(state.totalTo) : undefined
+            ]
+        };
+
+        return data.filter(row => compare(row, filterState));
     }
 }
